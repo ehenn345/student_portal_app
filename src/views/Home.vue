2ee83b7c535e82c7fe1a9e05f5f7684569f492b4
Emmanuel Daniels <emmanueldaniels37@gmail.com>
2020-12-28 22:32:31 -0500

    made drop down items for login page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,35 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Student Portal App</h1>
+    <div id='container' style="margin:50px auto 0; width:450px;">
+         <ejs-dropdownlist id='dropdownlist' :dataSource='loginData' placeholder='Login'></ejs-dropdownlist>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Vue from "vue";
+import { DropDownListPlugin } from "@syncfusion/ej2-vue-dropdowns";
+Vue.use(DropDownListPlugin);
 
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+export default Vue.extend({
+  data: function () {
+    return {
+      loginData: [
+        "Student Name1",
+        "Student Name2",
+        "Student Name3",
+        "Student Name4",
+        "Student Name5",
+      ],
+    };
+  },
+});
 </script>
+
+<style>
+@import "../../node_modules/@syncfusion/ej2-base/styles/material.css";
+@import "../../node_modules/@syncfusion/ej2-inputs/styles/material.css";
+@import "../../node_modules/@syncfusion/ej2-vue-dropdowns/styles/material.css";
+</style>
